78e59384728f41dd990528e06efa77a3006668f9
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-11-27 11:54:19 +0530

    _ is alphanumeric

--- src/lexer.js
+++ src/lexer.js
@@ -57,7 +57,7 @@ class SymbolizerIO {
         let code = this.at().charCodeAt(0);
         return (code >= 48 && code <= 57) ||
             (code >= 65 && code <= 90) ||
-            (code >= 97 && code <= 122);
+            (code >= 97 && code <= 122) || code === 95;
     }
 }
 export function symbolize(input) {
