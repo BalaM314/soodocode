31e57551ec0c114f299fc17a16c17e1cc179965e
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-12-14 22:49:22 +0530

    Basic expression parsing

--- src/runtime.js
+++ src/runtime.js
@@ -1,4 +1,5 @@
 import { crash } from "./utils.js";
+import { fail } from "./utils.js";
 export class Runtime {
     constructor(_input, _output) {
         this._input = _input;
@@ -9,7 +10,26 @@ export class Runtime {
         this.files = {};
     }
     evaluateExpr(expr) {
-        crash(`TODO`);
+        if ("operator" in expr) {
+            switch (expr.operator) {
+                default: crash("TODO parse expressions with operators");
+            }
+        }
+        else {
+            switch (expr.type) {
+                case "boolean.false": return false;
+                case "boolean.true": return false;
+                case "number.decimal": return Number(expr.text);
+                case "string": return expr.text.slice(1, -1); //remove the quotes
+                case "name":
+                    if (!(expr.text in this.variables))
+                        fail(`Undeclared variable ${expr.text}`);
+                    if (this.variables[expr.text].value == null)
+                        fail(`Cannot use the value of uninitialized variable ${expr.text}`);
+                    return this.variables[expr.text].value;
+                default: fail(`Cannot evaluate token of type ${expr.type}`);
+            }
+        }
     }
     runBlock(code) {
         for (const line of code) {
--- src/runtime.ts
+++ src/runtime.ts
@@ -1,6 +1,7 @@
 import type { ExpressionAST, ExpressionASTTreeNode, ProgramAST } from "./parser.js";
 import type { ConstantStatement, DeclarationStatement } from "./statements.js";
 import { crash } from "./utils.js";
+import { fail } from "./utils.js";
 
 interface FileData {
 	name: string;
@@ -37,7 +38,23 @@ export class Runtime {
 		public _output: (message:string) => void,
 	){}
 	evaluateExpr(expr:ExpressionAST):VariableValueType {
-		crash(`TODO`);
+		if("operator" in expr){
+			switch(expr.operator){
+				default: crash("TODO parse expressions with operators");
+			}
+		} else {
+			switch(expr.type){
+				case "boolean.false": return false;
+				case "boolean.true": return false;
+				case "number.decimal": return Number(expr.text);
+				case "string": return expr.text.slice(1, -1); //remove the quotes
+				case "name":
+					if(!(expr.text in this.variables)) fail(`Undeclared variable ${expr.text}`);
+					if(this.variables[expr.text].value == null) fail(`Cannot use the value of uninitialized variable ${expr.text}`);
+					return this.variables[expr.text].value!;
+				default: fail(`Cannot evaluate token of type ${expr.type}`);
+			}
+		}
 	}
 	runBlock(code:ProgramAST){
 		for(const line of code){
