b5822579c9802a8b444f795d9c3f578d91443e44
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-12-14 16:27:18 +0530

    clean

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
 "use strict";
-var VariableType;
-(function (VariableType) {
-    VariableType["INTEGER"] = "INTEGER";
-    VariableType["REAL"] = "REAL";
-    VariableType["STRING"] = "STRING";
-    VariableType["CHAR"] = "CHAR";
-    VariableType["BOOLEAN"] = "BOOLEAN";
-    VariableType["DATE"] = "DATE";
-})(VariableType || (VariableType = {}));
-var FileMode;
-(function (FileMode) {
-    FileMode["READ"] = "READ";
-    FileMode["WRITE"] = "WRITE";
-    FileMode["APPEND"] = "APPEND";
-})(FileMode || (FileMode = {}));
-function executeLine(line, context) {
-}
 let firstRun = false;
