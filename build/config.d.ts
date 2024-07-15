30156b6a8fd3571cde9173989a4a926175dd8441
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-07-15 20:34:29 +0530

    Fix it further

--- build/config.d.ts
+++ build/config.d.ts
@@ -1,32 +1,71 @@
-export type Config<T> = {
+export type Config<T, Help extends boolean> = {
     name: string;
     description: string | null;
-    errorHelp: string;
     value: T;
     defaultValue: T;
-};
+} & (Help extends true ? {
+    errorHelp: string;
+} : {});
 export declare const configs: {
     coercion: {
-        arrays_same_length: Config<boolean>;
+        arrays_same_length: Config<boolean, true>;
+        enums_to_integer: Config<boolean, true>;
+        string_to_char: Config<boolean, true>;
+        numbers_to_string: Config<boolean, true>;
+        enums_to_string: Config<boolean, true>;
+        booleans_to_string: Config<boolean, true>;
+        char_to_string: Config<boolean, true>;
+        date_to_string: Config<boolean, true>;
+        arrays_to_string: Config<boolean, true>;
     };
     arrays: {
-        unspecified_length: Config<boolean>;
+        unspecified_length: Config<boolean, true>;
     };
     initialization: {
-        normal_variables_default: Config<boolean>;
-        arrays_default: Config<boolean>;
+        normal_variables_default: Config<boolean, true>;
+        arrays_default: Config<boolean, true>;
     };
     default_values: {
-        INTEGER: Config<number>;
-        REAL: Config<number>;
-        BOOLEAN: Config<boolean>;
-        STRING: Config<string>;
-        CHAR: Config<string>;
-        DATE: Config<Date>;
+        INTEGER: {
+            name: string;
+            description: string | null;
+            value: number;
+            defaultValue: number;
+        };
+        REAL: {
+            name: string;
+            description: string | null;
+            value: number;
+            defaultValue: number;
+        };
+        BOOLEAN: {
+            name: string;
+            description: string | null;
+            value: boolean;
+            defaultValue: boolean;
+        };
+        STRING: {
+            name: string;
+            description: string | null;
+            value: string;
+            defaultValue: string;
+        };
+        CHAR: {
+            name: string;
+            description: string | null;
+            value: string;
+            defaultValue: string;
+        };
+        DATE: {
+            name: string;
+            description: string | null;
+            value: Date;
+            defaultValue: Date;
+        };
     };
     statements: {
-        call_functions: Config<boolean>;
-        auto_declare_classes: Config<boolean>;
+        call_functions: Config<boolean, true>;
+        auto_declare_classes: Config<boolean, true>;
     };
     misc: {};
 };
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 
-export type Config<T, Help> = {
+export type Config<T, Help extends boolean> = {
 	name:string;
 	description:string | null;
 	value:T;
@@ -24,10 +24,10 @@ export const configs = (<T extends Record<string, Record<string, ConfigData<unkn
 		[k1, Object.fromEntries(Object.entries(v).map(([k2, v]) =>
 			[k2, {
 				name: v.name,
-				description: "description" in v ? v.description : "shortDescription" in v ? v.shortDescription + "." + (v.fullDescription ? "\n" + v.fullDescription : "") : null,
+				description: "description" in v ? v.description : "shortDescription" in v ? v.shortDescription + (v.fullDescription ? "\n" + v.fullDescription : "") : null,
 				defaultValue: v.value,
 				value: v.value,
-				errorHelp: "errorHelp" in v ? `help: ${v.errorHelp}` : "shortDescription" in v ? `help: ${v.shortDescription} by enabling the config "${v.name}"` : null,
+				errorHelp: "errorHelp" in v ? `help: ${v.errorHelp} by enabling the config "${v.name}"` : "shortDescription" in v ? `help: ${v.shortDescription.replace(/\.$/, "")} by enabling the config "${v.name}"` : null,
 			}]
 		))]
 	)) as {
@@ -39,7 +39,7 @@ export const configs = (<T extends Record<string, Record<string, ConfigData<unkn
 	coercion: {
 		arrays_same_length: {
 			name: "Coerce arrays of same length",
-			shortDescription: `Allow assigning array types with same lengths but different start and end indexes`,
+			shortDescription: `Allow assigning array types with same lengths but different start and end indexes.`,
 			fullDescription: `For example, "ARRAY[1:10] OF INTEGER" can be coerced to "ARRAY[0:9] OF INTEGER".`,
 			value: true,
 		}
@@ -47,7 +47,7 @@ export const configs = (<T extends Record<string, Record<string, ConfigData<unkn
 	arrays: {
 		unspecified_length: {
 			name: "Unspecified length arrays",
-			shortDescription: `Allow arrays without a length in function arguments and class properties`,
+			shortDescription: `Allow arrays without a length in function arguments and class properties.`,
 			fullDescription: `This isn't fully variable length arrays, like in Python and Javascript: all arrays will still have a length, but functions can be more flexible.\nNot mentioned in any official pseudocode guides.`,
 			value: true
 		}
@@ -55,13 +55,13 @@ export const configs = (<T extends Record<string, Record<string, ConfigData<unkn
 	initialization: {
 		normal_variables_default: {
 			name: "Defaults for variables",
-			shortDescription: `Give uninitialized variables a default value`,
+			shortDescription: `Give uninitialized variables a default value.`,
 			fullDescription: `If disabled, uninitialized variables will throw an error upon being accessed.`,
 			value: false,
 		},
 		arrays_default: {
 			name: "Defaults for arrays",
-			shortDescription: `Automatically initialize all slots in arrays`,
+			shortDescription: `Automatically initialize all slots in arrays.`,
 			fullDescription: `If disabled, uninitialized slots will throw an error upon being accessed.`,
 			value: true,
 		}
@@ -95,13 +95,13 @@ export const configs = (<T extends Record<string, Record<string, ConfigData<unkn
 	statements: {
 		call_functions: {
 			name: "CALL functions",
-			shortDescription: `Allow using the CALL statement to call functions`,
+			shortDescription: `Allow using the CALL statement to call functions.`,
 			fullDescription: `Cambridge has explicitly stated that the CALL statement cannot be used to call functions, in section 8.2 of the official pseudocode guide.`,
 			value: false
 		},
 		auto_declare_classes: {
 			name: "Automatic class declaration",
-			shortDescription: `Automatically declare a variable when initializing a class in an assignment statement`,
+			shortDescription: `Automatically declare a variable when initializing a class in an assignment statement.`,
 			fullDescription: `If a variable has already been declared, that variable is used instead.`,
 			value: false
 		},
