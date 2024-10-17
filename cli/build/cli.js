76be6256ddef3d5c593af162125347bf447d530e
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-10-17 11:51:44 +0530

    Trim shebang

--- cli/build/cli.js
+++ cli/build/cli.js
@@ -81,8 +81,11 @@ class NodeJSFileSystem {
 const soodocode = new Application("soodocode", "CLI interface for the Soodocode runtime");
 soodocode.command("run", "Runs a soodocode file.", async (opts, app) => {
     const filename = opts.positionalArgs[0];
-    const data = await fsP.readFile(filename, "utf-8")
+    let data = await fsP.readFile(filename, "utf-8")
         .catch(() => fail(`Failed to read the file "${filename}"`));
+    if (data.startsWith("#!")) {
+        data = data.replace(/^#!.*\n/, "");
+    }
     const runtime = new Runtime(function input(message, type) {
         return promptSync();
     }, function output(values) {
